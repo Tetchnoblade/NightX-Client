626da0a02ce82f7b3d9703c1bd10a21f8d62fdf1
Aspw <0kcabtuo0@gmail.com>
2023-01-28 21:15:37 +0900

    more items

--- src/main/java/net/aspw/nightx/features/special/ModItems.kt
+++ src/main/java/net/aspw/nightx/features/special/ModItems.kt
@@ -5,7 +5,6 @@ import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.init.Blocks
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import java.util.*
 
 class ModItems : CreativeTabs("Mod Items") {
     override fun displayAllReleventItems(itemList: MutableList<ItemStack>) {
@@ -30,11 +29,10 @@ class ModItems : CreativeTabs("Mod Items") {
         itemList.add(ItemUtils.createItem("dispenser 64 0 {BlockEntityTag:{Items:[],id:\"minecraft:dispenser\",Lock:\"鍵\",},}"))
         itemList.add(ItemUtils.createItem("dropper 64 0 {BlockEntityTag:{Items:[],id:\"minecraft:dropper\",Lock:\"鍵\",},}"))
         itemList.add(ItemUtils.createItem("hopper 64 0 {BlockEntityTag:{Items:[],id:\"minecraft:hopper\",Lock:\"鍵\",},}"))
+        itemList.add(ItemUtils.createItem("mob_spawner 64 0 {BlockEntityTag:{MaxNearbyEntities:1000,RequiredPlayerRange:100,SpawnCount:100,SpawnData:{entity:{Motion:[0:0.0d,1:0.0d,2:0.0d,],BlockState:{Name:\"minecraft:spawner\",},Block:\"minecraft:mob_spawner\",Time:1,id:\"minecraft:falling_block\",TileEntityData:{EntityId:\"FallingSand\",MaxNearbyEntities:1000,RequiredPlayerRange:100,SpawnCount:100,SpawnData:{Motion:[0:0.0d,1:0.0d,2:0.0d,],Block:\"mob_spawner\",Time:1,Data:0,TileEntityData:{EntityId:\"EnderDragon\",MaxNearbyEntities:1000,RequiredPlayerRange:100,SpawnCount:100,MaxSpawnDelay:20,SpawnRange:100,MinSpawnDelay:20,},DropItem:0,},MaxSpawnDelay:20,SpawnRange:500,MinSpawnDelay:20,},DropItem:0,},},id:\"minecraft:mob_spawner\",MaxSpawnDelay:5,SpawnRange:500,Delay:20,MinSpawnDelay:5,},display:{Name:\"§4Server Crasher\",},}"))
+        itemList.add(ItemUtils.createItem("potion 64 0 {CustomPotionEffects:[0:{Ambient:0b,ShowIcon:1b,ShowParticles:1b,Duration:19999980,Id:10b,Amplifier:125b,},1:{Ambient:0b,ShowIcon:1b,ShowParticles:1b,Duration:19999980,Id:11b,Amplifier:125b,},2:{Ambient:0b,ShowIcon:1b,ShowParticles:1b,Duration:19999980,Id:22b,Amplifier:4b,},],CustomPotionColor:16711680,display:{Name:\"§6God Potion\",},}"))
         itemList.add(ItemUtils.createItem("name_tag 64 0 {display:{Name: \"$lagStringBuilder\"}}"))
-        itemList.add(
-            Objects.requireNonNull(ItemUtils.createItem("fireworks 64 0 {HideFlags:63,Fireworks:{Flight:127b,Explosions:[0:{Type:0b,Trail:1b,Colors:[16777215,],Flicker:1b,FadeColors:[0,]}]}}"))
-                .setStackDisplayName("§cInfinite §a§lFirework")
-        )
+        itemList.add(ItemUtils.createItem("fireworks 64 0 {HideFlags:63,Fireworks:{Flight:127b,Explosions:[0:{Type:0b,Trail:1b,Colors:[16777215,],Flicker:1b,FadeColors:[0,]}]}}"))
     }
 
     override fun getTabIconItem(): Item = ItemStack(Blocks.command_block).item
