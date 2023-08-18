050ccb7e483362a47e1d900fca52424e1ee8e218
Aspw-w <0kcabtuo0@gmail.com>
2023-08-19 01:48:00 +0900

    Semidiz

--- src/main/java/net/aspw/client/features/module/impl/movement/speeds/other/Minemen.java
+++ src/main/java/net/aspw/client/features/module/impl/movement/speeds/other/Minemen.java
@@ -6,31 +6,24 @@ import net.aspw.client.features.module.impl.movement.speeds.SpeedMode;
 import net.aspw.client.util.MovementUtils;
 
 /**
- * The type Hive hop.
+ * The type Minemen hop.
  */
 public class Minemen extends SpeedMode {
 
-    /**
-     * Instantiates a new Hive hop.
-     */
     public Minemen() {
         super("Minemen");
     }
 
-    @EventTarget
-    public void onStrafe() {
-        if (mc.thePlayer.hurtTime <= 6)
-            MovementUtils.strafe();
-    }
-
     @Override
     public void onMotion() {
-        if (mc.thePlayer.onGround)
+        if (mc.thePlayer.onGround && MovementUtils.isMoving())
             mc.thePlayer.jump();
     }
 
     @Override
     public void onUpdate() {
+        if (mc.thePlayer.hurtTime <= 6)
+            MovementUtils.strafe();
     }
 
     @Override
