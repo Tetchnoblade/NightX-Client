17f6ee9f9caa9dead5c03c74398c3fd46108628d
Aspw-w <0kcabtuo0@gmail.com>
2023-10-28 08:30:12 +0900

    a;;bacl

--- src/main/java/net/aspw/client/injection/forge/mixins/performance/MixinWorldManager.java
+++ src/main/java/net/aspw/client/injection/forge/mixins/performance/MixinWorldManager.java
@@ -3,16 +3,11 @@ package net.aspw.client.injection.forge.mixins.performance;
 import net.aspw.client.injection.access.IMixinWorldAccess;
 import net.minecraft.world.WorldManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = WorldManager.class)
 public abstract class MixinWorldManager implements IMixinWorldAccess {
 
-    @Shadow
-    protected abstract void markBlocksForUpdate(int var1, int var2, int var3, int var4, int var5, int var6);
-
     @Override
     public void notifyLightSet(int n, int n2, int n3) {
-        this.markBlocksForUpdate(n - 1, n2 - 1, n3 - 1, n + 1, n2 + 1, n3 + 1);
     }
 }
