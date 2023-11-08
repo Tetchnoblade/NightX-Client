9293c658ca59726c8763488b50c08b81c5911780
BlueLatios <65719723+LatiBlueLatios@users.noreply.github.com>
2023-11-08 11:42:34 -0500

    Create newPacketUtils.kt

---
+++ src/main/java/net/aspw/client/util/newPacketUtils.kt
@@ -0,0 +1,113 @@
+// I'm not sure how much of an impact changing the original 
+// PacketUtils will do
+package net.aspw.client.util
+
+import net.minecraft.network.NetworkManager
+import net.minecraft.network.Packet
+import net.minecraft.network.play.INetHandlerPlayClient
+import net.minecraft.network.play.server.S08PacketPlayerPosLook
+import net.minecraft.network.play.server.S0CPacketSpawnPlayer
+import net.minecraft.network.play.server.S0EPacketSpawnObject
+import net.minecraft.network.play.server.S12PacketEntityVelocity
+import net.minecraft.network.play.server.S14PacketEntity
+import net.minecraft.network.play.server.S18PacketEntityTeleport
+import kotlin.math.roundToInt
+
+
+object PacketUtils : MinecraftInstance() {
+    // TODO: Remove annotations once all modules are converted to kotlin.
+    @JvmStatic
+    @JvmOverloads
+    fun sendPacket(packet: Packet<*>, triggerEvent: Boolean = true) {
+        if (triggerEvent) {
+            mc.netHandler?.addToSendQueue(packet)
+            return
+        }
+
+        val netManager = mc.netHandler?.networkManager ?: return
+        if (netManager.isChannelOpen) {
+            netManager.flushOutboundQueue()
+            netManager.dispatchPacket(packet, null)
+        } else {
+            netManager.readWriteLock.writeLock().lock()
+            try {
+                netManager.outboundPacketsQueue += NetworkManager.InboundHandlerTuplePacketListener(packet, null)
+            } finally {
+                netManager.readWriteLock.writeLock().unlock()
+            }
+        }
+    }
+
+    @JvmStatic
+    @JvmOverloads
+    fun sendPackets(vararg packets: Packet<*>, triggerEvents: Boolean = true) =
+        packets.forEach { sendPacket(it, triggerEvents) }
+
+    fun handlePackets(vararg packets: Packet<*>) =
+        packets.forEach { handlePacket(it) }
+
+    fun handlePacket(packet: Packet<*>?) =
+        runCatching { (packet as Packet<INetHandlerPlayClient>).processPacket(mc.netHandler) }
+
+    val Packet<*>.type
+        get() = when (this.javaClass.simpleName[0]) {
+            'C' -> PacketType.CLIENT
+            'S' -> PacketType.SERVER
+            else -> PacketType.UNKNOWN
+        }
+
+    enum class PacketType { CLIENT, SERVER, UNKNOWN }
+}
+
+var S12PacketEntityVelocity.realMotionX
+    get() = motionX / 8000.0
+    set(value) {
+        motionX = (value * 8000.0).roundToInt()
+    }
+var S12PacketEntityVelocity.realMotionY
+    get() = motionY / 8000.0
+    set(value) {
+        motionX = (value * 8000.0).roundToInt()
+    }
+var S12PacketEntityVelocity.realMotionZ
+    get() = motionZ / 8000.0
+    set(value) {
+        motionX = (value * 8000.0).roundToInt()
+    }
+
+val S14PacketEntity.realMotionX
+    get() = func_149062_c() / 32.0
+val S14PacketEntity.realMotionY
+    get() = func_149061_d() / 32.0
+val S14PacketEntity.realMotionZ
+    get() = func_149064_e() / 32.0
+
+var S0EPacketSpawnObject.realX
+    get() = x / 32.0
+    set(value) {
+        x = (value * 32.0).roundToInt()
+    }
+var S0EPacketSpawnObject.realY
+    get() = y / 32.0
+    set(value) {
+        y = (value * 32.0).roundToInt()
+    }
+var S0EPacketSpawnObject.realZ
+    get() = z / 32.0
+    set(value) {
+        z = (value * 32.0).roundToInt()
+    }
+
+val S0CPacketSpawnPlayer.realX
+    get() = x / 32.0
+val S0CPacketSpawnPlayer.realY
+    get() = y / 32.0
+val S0CPacketSpawnPlayer.realZ
+    get() = z / 32.0
+
+val S18PacketEntityTeleport.realX
+    get() = x / 32.0
+val S18PacketEntityTeleport.realY
+    get() = y / 32.0
+val S18PacketEntityTeleport.realZ
+    get() = z / 32.0
