3f1f6d0833669943e412b4da5fee1641eae49ddb
Aspw-w <0kcabtuo0@gmail.com>
2023-11-12 00:50:04 +0900

    verus flagrless damage longjump

--- src/main/java/net/aspw/client/features/module/impl/minigames/TestModule2.kt
+++ src/main/java/net/aspw/client/features/module/impl/minigames/TestModule2.kt
@@ -4,16 +4,28 @@ import net.aspw.client.event.*
 import net.aspw.client.features.module.Module
 import net.aspw.client.features.module.ModuleCategory
 import net.aspw.client.features.module.ModuleInfo
+import net.aspw.client.util.MovementUtils
+import net.aspw.client.util.misc.RandomUtils
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.client.C0BPacketEntityAction
+import net.minecraft.network.play.server.S12PacketEntityVelocity
 
 @ModuleInfo(name = "TestModule2", spacedName = "Test Module 2", description = "", category = ModuleCategory.MINIGAMES)
 class TestModule2 : Module() {
     override val tag: String
-        get() = "ZOZOLongBeta"
+        get() = "Verus Flagless DMG HighJump Beta"
+
+    var started = false
 
     override fun onEnable() {
     }
 
     override fun onDisable() {
+        mc.thePlayer.motionX = 0.0
+        mc.thePlayer.motionY = 0.0
+        mc.thePlayer.motionZ = 0.0
+        mc.timer.timerSpeed = 1.0f
+        started = false
     }
 
     @EventTarget
@@ -34,6 +46,13 @@ class TestModule2 : Module() {
 
     @EventTarget
     fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        if (mc.thePlayer.hurtTime > 0 && (packet is C03PacketPlayer || packet is C0BPacketEntityAction)) {
+            event.cancelEvent()
+        }
+        if (packet is S12PacketEntityVelocity) {
+            event.cancelEvent()
+        }
     }
 
     @EventTarget
@@ -42,6 +61,26 @@ class TestModule2 : Module() {
 
     @EventTarget
     fun onMove(event: MoveEvent) {
+        if (mc.thePlayer.hurtTime > 0) {
+            started = true
+            if (mc.thePlayer.hurtTime > 3) {
+                event.y += 0.7
+                event.zeroXZ()
+                return
+            }
+            chat("AcceptVelo-" + RandomUtils.randomNumber(9))
+            if (mc.thePlayer.hurtTime > 2)
+                MovementUtils.strafe(2f)
+        } else {
+            mc.timer.timerSpeed = 1.0f
+            started = false
+        }
+        if (started) {
+            mc.timer.timerSpeed = 0.3f
+            event.y += 1.2
+        } else {
+            mc.timer.timerSpeed = 1.0f
+        }
     }
 
     @EventTarget
